0df690316fbc279b28688bafaa0934de977bc20a
Zaidamaan86 <Zaidamaan86@gmail.com>
2025-02-01 22:33:09 +0530

    users Table Succefully Created and tested in DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from sqlalchemy import inspect
 
 db = SQLAlchemy()
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-db.init_app(app)
+    db.init_app(app)
 
-with app.app_context():
-    db.create_all()
-    print("Database initialized.")
+    with app.app_context():
+        from app.models import User
+        db.create_all()
+        print("Database initialized.")
 
-# Import routes after initializing the app
-from app.routes.landing_page import landing_bp
-from app.routes.signup import signup_bp
-from app.routes.login import login_bp
-from app.routes.delete import delete_bp
 
-# Register blueprints
-app.register_blueprint(login_bp)
-app.register_blueprint(landing_bp)
-app.register_blueprint(delete_bp)
-app.register_blueprint(signup_bp)
+        # Import routes after initializing the app
+        from app.routes.landing_page import landing_bp
+        from app.routes.signup import signup_bp
+        from app.routes.login import login_bp
+        from app.routes.delete import delete_bp
+
+        # Register blueprints
+        app.register_blueprint(login_bp)
+        app.register_blueprint(landing_bp)
+        app.register_blueprint(delete_bp)
+        app.register_blueprint(signup_bp)
+
+    return app
 
 
--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-from app import app, db
+from app import create_app, db
+
+
+app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        try:
-            db.create_all()
-            print("Tables created successfully")
-        except Exception as e:
-            print(f"Error creating tables: {e}")
-
-    app.run(debug=True, port='5050')
+    app.run(debug=True)
