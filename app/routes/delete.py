9e8661a105fccf3e7ce84116893b6d205fdcd12c
Md Zeeshan <mdzeeshanbtech@gmail.com>
2025-02-03 21:19:52 +0530

    Delete Page Added

--- app/routes/delete.py
+++ app/routes/delete.py
@@ -1,10 +1,18 @@
-from flask import Blueprint, render_template, request, url_for
-
+from flask import Blueprint, request, url_for, redirect, flash
+from app.models import User  # Import the User model
+from app import db  # Import the database instance
 
 delete_bp = Blueprint('delete', __name__)
 
-
-@delete_bp.route('/login/delete')
+@delete_bp.route('/login/delete', methods=['POST'])
 def delete():
-    return "Delete User"
-    
+    username = request.form.get('username')
+    user = User.query.filter_by(username=username).first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        flash('Account deleted successfully.')
+        return redirect(url_for('landing.landing'))
+    else:
+        flash('Account not found.')
+        return redirect(url_for('login.login'))
